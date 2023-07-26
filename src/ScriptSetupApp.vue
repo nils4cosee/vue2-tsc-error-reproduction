d708c86e1fbb9fe0b344a8de7d4894b3875e8039
Nils Knappmeier <nils.knappmeier@cosee.biz>
2023-07-26 16:45:12 +0200

    add ScriptSetupApp

---
+++ src/ScriptSetupApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Range: {{ range.min }} - {{ range.max }}
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const range = ref({min: 0, max: 10})
+
+</script>
+
